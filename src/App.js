d1865237f0e547fca8f1cbb437b7d7e594b58d27
Julio Navarro <jnavarro@able.co>
2020-07-07 19:57:06 -0500

    Add CSS reset

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 
 import React from "react";
 import { css, jsx, Global } from "@emotion/core";
+import styled from "@emotion/styled";
 import CSSReset from "./components/CSSReset";
 
 function App() {
   return (
     <div>
       <CSSReset />
-
     </div>
   );
 }
--- src/components/CSSReset.js
+++ src/components/CSSReset.js
@@ -7,11 +7,6 @@ function CSSReset() {
   return (
     <Global
       styles={css`
-        /* http://meyerweb.com/eric/tools/css/reset/ 
-   v2.0 | 20110126
-   License: none (public domain)
-*/
-
         html,
         body,
         div,
